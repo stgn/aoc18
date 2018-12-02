15d92a5fdea16ce17361b341950fc6ef340d0f0b
shane <181157+stgn@users.noreply.github.com>
2018-12-02 11:38:09 -0500

    Add Day 2 solutions

---
+++ 02.py
@@ -0,0 +1,26 @@
+import sys
+from collections import Counter
+from itertools import combinations
+
+
+def part_one(ids):
+    counts = (Counter(x).values() for x in ids)
+    present = ((2 in c, 3 in c) for c in counts)
+    two, three = zip(*present)
+    return sum(two) * sum(three)
+
+
+def part_two(ids):
+    for a, b in combinations(ids, 2):
+        same = ''.join(x for x, y in zip(a, b) if x == y)
+        if len(same) == len(a) - 1:
+            return same
+
+
+if __name__ == '__main__':
+    fn, = sys.argv[1:]
+    with open(fn) as f:
+        ids = [x.strip() for x in f]
+
+    print(part_one(ids))
+    print(part_two(ids))
